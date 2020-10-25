68529211e7776cc6b579dc7d7c8bcd947b0a1768
Chris Oyer <chrisoyer@users.noreply.github.com>
2020-10-25 13:31:10 -0600

    fix: rewrote parse to use named xpath, started writing method to parse artist page

--- src/data/artist_scrape.py
+++ src/data/artist_scrape.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import scrapy
+from scrapy.shell import inspect_response
 from scrapy.crawler import CrawlerProcess
 from scrapy.linkextractors import LinkExtractor
 from string import Template
@@ -9,41 +10,39 @@ import pandas as pd
 
 process = CrawlerProcess({'AUTOTHROTTLE_ENABLED': True, 
                           'AUTOTHROTTLE_TARGET_CONCURRENCY': .20,
-                          'HTTPCACHE_ENABLED': True,  # remove for final scrape to get live data
-                          'ROBOTSTXT_OBEY': True,
+                          'HTTPCACHE_ENABLED': False,  # remove for final scrape to get live data
+                          'ROBOTSTXT_OBEY': False,
                           'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
-                          'FEED_URI': '../../data/raw/artists_data.json',
-                          'FEED_FORMAT': 'json',
+                          'FEEDS': {'../../data/raw/artists_data.json': {'format': 'json'}},
                           'FEED_EXPORT_ENCODING': '"utf-8"'})
 
 class RymArtistSpider(scrapy.Spider):
     name = 'RymArtists'
-    pages = range(1, 2)  # set to 25 once multipage is working
-    start_yr = 1
-    stop_yr = 2  # sb 25 eventually 
-    years = range(start_yr, stop_yr)
-    starturls = [f"https://rateyourmusic.com/charts/top/album/{year}/{page}"
-                 for page in pages for year in years]
+    pages = list(range(1, 2))  # set to 25 once multipage is working
+    start_yr = 1950
+    stop_yr = 1951  # sb 25 eventually 
+    start_urls = [f"https://rateyourmusic.com/charts/top/album/{year}/{page}"
+                 for page in pages for year in range(1950,1951)]
 
     def parse(self, response):
+        """Parses yearly top album chart pages to obtain artists w/urls"""
         if response.status == 404:
             return 
-        artists_dct = {}
-        album_xp = r'//*[@id="content"]/table/tbody/tr/td/table/tbody/'
-        artist_xp = r'td[3]/div[1]/div[2]/span/a'
-        multiartist_xp = r"td[3]/div[1]/div[2]/span/span[2]/div/div/"
-        for album in response.xpath(album_xp):
-            # multiple artists e.g. colab:
-            if response.xpath(multiartist_xp):
-                for mult_artist in album.xpath(mutiartist_xp):
-                    artist_name = mult_artist.xpath("@class").get()
-                    artist_url = mult_artists.xpath("@href").get()
-                    artists_dct[artist_name] = artist_url
-            else:
-                artist_name = album.xpath(artist_xp+"@class").get()
-                artist_url = album.xpath(artist_xp+"@href").get()
-            artists_dct[artist_name] = artist_url
-        yield artists_dct
+        artist_xp = r'//a[has-class("artist")/'
+        artist_name_xp = artist_xp + 'text()'
+        artist_url_xp = artist_xp + '@href'
+        artist_names = response.xpath(artist_xp).getall()
+        artist_urls = response.xpath(artists_url_xp).getall()
+        # scrapy doesn't crawl dups by default, but this will prefilter anyway
+        artists_dct = {name: 'rateyourmusic.com/' + url 
+                for name, url in zip(artist_names, artist_urls)}
+        for artist, artist_url in artists_dict.items():
+            yield scrapy.Request(artist_url, callback=artistparse,
+                    meta={'artist': artist})
+
+    def artistparse(self, response)
+        artist=response.meta.get('artist')
+
 
 process.crawl(RymArtistSpider)
 process.start()
