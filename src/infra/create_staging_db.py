81bd0e56aa6c73409ec5b35356b96b52193e9c82
Chris Oyer <chrisoyer@users.noreply.github.com>
2020-11-24 17:20:16 -0500

    ft: added script to create staging db

--- src/infra/create_staging_db.py
+++ src/infra/create_staging_db.py
@@ -4,37 +4,47 @@ import boto3
 
 rds = boto3.client('rds')
 
-cpg = rds.create_db_cluster_parameter_group(
-    DBClusterParameterGroupName='staging',
-    DBParameterGroupFamily='aurora5.6',
-    Description='for loading raw data',
-    Tags=[
-        {
+# only need to do this once
+try: 
+    cpg = rds.create_db_cluster_parameter_group(
+        DBClusterParameterGroupName='music-pipeline',
+        DBParameterGroupFamily='aurora5.6',
+        Description='for loading raw data',
+        Tags=[
+            {
             'Key': 'Name',
             'Value': 'Staging'
-        },
-    ]
-)
+            },
+        ]
+    )
+except:
+    pass
 
 db_cluster = rds.create_db_cluster(
     AvailabilityZones=[
         'us-east-1a',
     ],
     BackupRetentionPeriod=1,
-    DBClusterIdentifier='mydbcluster1',
-    DBClusterParameterGroupName='staging',
+    DBClusterIdentifier='music-pipeline-staging',
+    DBClusterParameterGroupName='music-pipeline',
     DatabaseName='staging1',
     Engine='aurora',
-    EngineVersion='5.6.10a',
+    EngineMode='serverless',
+    ScalingConfiguration={
+        'MinCapacity': 1,
+        'MaxCapacity': 1, # since we are just doing scraping the total data will be low
+        'AutoPause': True,
+        'SecondsUntilAutoPause': 300,
+        },
     MasterUserPassword=input('my password:'),
-    MasterUsername=staging_admin,
+    MasterUsername='root',
     Port=3306,
     StorageEncrypted=True,
 )
 
-instance = rds.create_db_instance(
-  DBInstanceIdentifier = 'staging_instance1',
-  DBInstanceClass = 'db.t3.medium', # smallest standard instance
-  Engine='aurora',
-  DBClusterIdentifier='staging_cluster1' # this is where you put the cluster name
-)
+#instance = rds.create_db_instance(
+#  DBInstanceIdentifier = 'staging-instance1a',
+#  DBInstanceClass = 'db.t3.medium', # smallest standard instance
+#  Engine='aurora',
+#  DBClusterIdentifier='music-pipeline-staging' # this is where you put the cluster name
+#)
--- src/infra/create_staging_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-aws rds create-db-cluster --db-cluster-identifier staging-cluster --engine aurora-postgresql --engine-version 10.12 \
---engine-mode serverless --scaling-configuration MinCapacity=8,MaxCapacity=64,SecondsUntilAutoPause=1000,AutoPause=true \
---master-username chrisoyer
